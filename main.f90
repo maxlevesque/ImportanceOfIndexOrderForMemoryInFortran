588f2fdf802e4f6592c8cb53f3b77a1740069711
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-11-13 17:14:17 +0100

    improve description. add test with semi-implicit

--- main.f90
+++ main.f90
@@ -1,69 +1,73 @@
 program main
 
-integer :: i, j, k
-integer, parameter :: n=700
-double precision, parameter :: s=1.119d0
-double precision :: a(n,n,n)
-double precision :: t0,t1,t2,t3,t4,t5,t6,t7,t8,t9
+  integer :: i, j, k
+  integer, parameter :: n=700
+  double precision, parameter :: s=acos(-1.d0)
+  double precision :: a(n,n,n)
+  real :: t(11), dtref
 
-call cpu_time(t0)
+  it=1
+  call cpu_time( t(it) )
 
-do i=1,n  
-    do j=1,n  
-        do k=1,n  
-            a(i,j,k)=s
-        end do
-    end do
-end do
+  do i=1,n
+      do j=1,n
+          do k=1,n
+              a(i,j,k)=s
+          end do
+      end do
+  end do
+  it = it+1
+  call cpu_time( t(it) )
+  dtref = t(it)-t(it-1)
+  print*,"do i; do j; do k; a(i,j,k)=s; needs",t(it)-t(it-1),"seconds. Reference score:",int((t(it)-t(it-1))/dtref*1000)
 
-call cpu_time(t1)
+  do i=1,n
+      do j=1,n
+          do k=1,n
+              a(k,j,i)=s
+          end do
+      end do
+  end do
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"do i; do j; do k; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-print*,"i,j,k => a(i,j,k) :",t1-t0," sec => ",int(1*1000)
+  a(:,:,:)=s
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"implicit a(:,:,:)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-do i=1,n
-    do j=1,n
-        do k=1,n
-            a(k,j,i)=s
-        end do
-    end do
-end do
+  a=s
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"implicit a=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-call cpu_time(t2)
+  do concurrent( i=1:n, j=1:n, k=1:n )
+      a(i,j,k)=s
+  end do
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"do concurrent i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-print*,"i,j,k => a(k,j,i) :",t2-t1," sec => ",int((t2-t1)/(t1-t0)*1000)
+  do concurrent( i=1:n, j=1:n, k=1:n)
+      a(k,j,i)=s
+  end do
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"do concurrent i,j,k ; a(k,j,i)=s :",t5-t4,"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-a=s
+  forall( i=1:n, j=1:n, k=1:n )
+      a(i,j,k)=s
+  end forall
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"forall i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
-call cpu_time(t3)
-
-print*,"implicit :",t3-t2," sec => ",int((t3-t2)/(t1-t0)*1000)
-
-do concurrent( i=1:n, j=1:n, k=1:n )
-    a(i,j,k)=s
-end do
-
-call cpu_time(t4)
-print*,"concurrent i,j,k => a(i,j,k) :",t4-t3," sec => ",int((t4-t3)/(t1-t0)*1000)
-
-do concurrent( i=1:n, j=1:n, k=1:n)
-    a(k,j,i)=s
-end do
-
-call cpu_time(t5)
-print*,"concurrent i,j,k => a(k,j,i) :",t5-t4," sec => ",int((t5-t4)/(t1-t0)*1000)
-
-forall( i=1:n, j=1:n, k=1:n )
-    a(i,j,k)=s
-end forall
-
-call cpu_time(t6)
-print*,"forall i,j,k => a(i,j,k) :",t6-t5," sec => ",int((t6-t5)/(t1-t0)*1000)
-
-forall( i=1:n, j=1:n, k=1:n )
-    a(k,j,i)=s
-end forall
-
-call cpu_time(t7)
-print*,"forall i,j,k => a(k,j,i) :",t7-t6," sec => ",int((t7-t6)/(t1-t0)*1000)
+  forall( i=1:n, j=1:n, k=1:n )
+      a(k,j,i)=s
+  end forall
+  it = it+1
+  call cpu_time( t(it) )
+  print*,"forall i,j,k ; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
 
 end program main
