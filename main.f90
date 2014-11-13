ea3a0a4feb545937bc22067eddb189f8fd2e589f
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-11-13 17:20:32 +0100

    speedup instead of score

--- main.f90
+++ main.f90
@@ -30,44 +30,44 @@ program main
   end do
   it = it+1
   call cpu_time( t(it) )
-  print*,"do i; do j; do k; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"do i; do j; do k; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   a(:,:,:)=s
   it = it+1
   call cpu_time( t(it) )
-  print*,"implicit a(:,:,:)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"implicit a(:,:,:)=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   a=s
   it = it+1
   call cpu_time( t(it) )
-  print*,"implicit a=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"implicit a=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   do concurrent( i=1:n, j=1:n, k=1:n )
       a(i,j,k)=s
   end do
   it = it+1
   call cpu_time( t(it) )
-  print*,"do concurrent i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"do concurrent i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   do concurrent( i=1:n, j=1:n, k=1:n)
       a(k,j,i)=s
   end do
   it = it+1
   call cpu_time( t(it) )
-  print*,"do concurrent i,j,k ; a(k,j,i)=s :",t5-t4,"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"do concurrent i,j,k ; a(k,j,i)=s :",t5-t4,"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   forall( i=1:n, j=1:n, k=1:n )
       a(i,j,k)=s
   end forall
   it = it+1
   call cpu_time( t(it) )
-  print*,"forall i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"forall i,j,k ; a(i,j,k)=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
   forall( i=1:n, j=1:n, k=1:n )
       a(k,j,i)=s
   end forall
   it = it+1
   call cpu_time( t(it) )
-  print*,"forall i,j,k ; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Score:",int((t(it)-t(it-1))/dtref*1000)
+  print*,"forall i,j,k ; a(k,j,i)=s :",t(it)-t(it-1),"seconds. Speedup =",dtref/(t(it)-t(it-1))
 
 end program main
